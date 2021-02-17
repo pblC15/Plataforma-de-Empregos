5fcb5cfd3548c0c91a992007706aef59c3201ab9
Pablo Cassiano <pabloboyreis@hotmail.com>
2021-02-17 02:05:09 -0300

    Finalizando o buscador lateral

--- pesquisa.php
+++ pesquisa.php
@@ -21,7 +21,10 @@
             exit;
         }
     }
+
 ?>
+
+
 <!DOCTYPE html>
 <html lang='pt-br'>
     <head>
@@ -60,87 +63,96 @@
             <div class='container'>
              
                 <div class='anuncio-principal'>
-
-
-                    <h2>Vagas de <?php echo ucwords($_GET['f_name']); ?></h2>
-                    
-                    <?php 
-                        require_once '_function/functionTexto.php';
-            
-
-                        $nome_pesq = $_GET['f_name'];
-
-
-                        //Obtendo a pagina vinda da URL
-                        $pagina_atual = filter_input(INPUT_GET, 'pagina', FILTER_SANITIZE_NUMBER_INT);
-                        //Se tiver vazia seta o 1
-                        $pagina = (!empty($pagina_atual)) ? $pagina_atual : 1;
-                        //Quantidade de itens
-                        $qtd_item = 5;
-                        
-                        $inicio = ($qtd_item * $pagina) - $qtd_item;
-                        
-                        //Sql para seleciona tudo da tabela tb-cadastro onde nome da vaga tenha a variavel ordernar por data 
-                        $sql = "SELECT * FROM tb_cadastro WHERE nome_V LIKE '%$nome_pesq%' ORDER BY data_C DESC LIMIT $inicio, $qtd_item";
-
-                        $result = mysqli_query($conn, $sql);
-                        
-                        $resultado = mysqli_affected_rows($conn);
-
-                        if(empty($resultado)){
-
-                            echo "<p class='alerta-mensagem'>Não existe vagas com nome '".ucwords($_GET['f_name'])."'</p>";
-                        
-                        }
-
-                        while($exibe = mysqli_fetch_array($result)){
-
-                            
-
-                            echo "<div class='breve-vaga'>
-                              
-                                <div class='img-vaga'>
-                                <a href='anuncio-vaga.php?id=".$exibe['id']."'><img src='_imgs/rascunho/vagas-emprego.jpg'></a>
-                                </div>
-    
-                                <div class='desc-vaga'>
-                                    <h3>".$exibe['nome_V']."</h3>
-                                    <p><b>Localidade:</b> ".$exibe['nome_E']."</p>
-                                    <p><b>Beneficios:</b>".$exibe['salario_B']."</p>
-                                    <p><b>Descrição:</b> ".reduzindoTexto($exibe['descricao'])."...</p>
-                                    <a href='anuncio-vaga.php?id=".$exibe['id']."'>Ver mais...</a>
-                                </div>
-                                <div class='clear'></div>
-                            </div><!--FIM DA DIV BREVE-VAGA -->";
-
-                       
-                        }   
-                        echo "<div class='paginacao'>";
-                        //Contando quantos resultados tem na tabela
-                        $result_pg = "SELECT COUNT(id) AS num_result FROM tb_cadastro WHERE tipo_V = 'Emprego'";
-                        //Executando a query
-                        $query = mysqli_query($conn, $result_pg);
-                        //Transformando em array
-                        $result = mysqli_fetch_assoc($query);
-
-                        //Arredonadando a quantidade de paginas
-                        $qtd_pg = ceil($result['num_result'] / $qtd_item);
-
-                        echo "<a class='pri_pg' href='vagas.php?pagina=1'>Primeira</a>";
-
-
-                        for($i = 1; $i <= $qtd_pg; $i++){
                 
-                             if($i >= 1){
+                <?php 
 
-                                echo "<a class='num_pg' href='vagas.php?pagina=$i'>$i</a>";
+                require_once "_function/functionTexto.php";
+                require_once "conx.php";
+
+                    if(isset($_POST['f_name'])){
+
+                        echo "<h2>Vagas de ".ucwords($_POST['f_name'])."</h2>";
+
+                        $nome_pesq = $_POST['f_name'];
+
+                    }else if($_GET['f_name']){
+                        
+                        echo "<h2>Vagas de".ucwords($_GET['f_name'])."</h2>";
+                        $nome_pesq = $_GET['f_name'];
+                    }
+                
+
+
+                    //Obtendo a pagina vinda da URL
+                    $pagina_atual = filter_input(INPUT_GET, 'pagina', FILTER_SANITIZE_NUMBER_INT);
+                    //Se tiver vazia seta o 1
+                    $pagina = (!empty($pagina_atual)) ? $pagina_atual : 1;
+                    //Quantidade de itens
+                    $qtd_item = 5;
                     
-                            }
-                        }
-                        echo "<a class='ult_pg' href='vagas.php?pagina=$qtd_pg'>Último</a>";
-                        echo "</div>";
+                    $inicio = ($qtd_item * $pagina) - $qtd_item;
+                    
+                    //Sql para seleciona tudo da tabela tb-cadastro onde nome da vaga tenha a variavel ordernar por data 
+                    $sql = "SELECT * FROM tb_cadastro WHERE nome_V LIKE '%$nome_pesq%' ORDER BY data_C DESC LIMIT $inicio, $qtd_item";
 
-                    ?>
+                    $result = mysqli_query($conn, $sql);
+                    
+                    $resultado = mysqli_affected_rows($conn);
+
+                    if(empty($resultado)){
+
+                        echo "<p class='alerta-mensagem'>Não existe vagas com nome '".ucwords($nome_pesq)."'</p>";
+                    
+                    }
+
+                    while($exibe = mysqli_fetch_array($result)){
+
+                        
+
+                        echo "<div class='breve-vaga'>
+                            
+                            <div class='img-vaga'>
+                            <a href='anuncio-vaga.php?id=".$exibe['id']."'><img src='_imgs/rascunho/vagas-emprego.jpg'></a>
+                            </div>
+
+                            <div class='desc-vaga'>
+                                <h3>".$exibe['nome_V']."</h3>
+                                <p><b>Localidade:</b> ".$exibe['nome_E']."</p>
+                                <p><b>Beneficios:</b>".$exibe['salario_B']."</p>
+                                <p><b>Descrição:</b> ".reduzindoTexto($exibe['descricao'])."...</p>
+                                <a href='anuncio-vaga.php?id=".$exibe['id']."'>Ver mais...</a>
+                            </div>
+                            <div class='clear'></div>
+                        </div><!--FIM DA DIV BREVE-VAGA -->";
+
+                    
+                    }   
+                    echo "<div class='paginacao'>";
+                    //Contando quantos resultados tem na tabela
+                    $result_pg = "SELECT COUNT(id) AS num_result FROM tb_cadastro WHERE tipo_V = 'Emprego'";
+                    //Executando a query
+                    $query = mysqli_query($conn, $result_pg);
+                    //Transformando em array
+                    $result = mysqli_fetch_assoc($query);
+
+                    //Arredonadando a quantidade de paginas
+                    $qtd_pg = ceil($result['num_result'] / $qtd_item);
+
+                    echo "<a class='pri_pg' href='vagas.php?pagina=1'>Primeira</a>";
+
+
+                    for($i = 1; $i <= $qtd_pg; $i++){
+            
+                            if($i >= 1){
+
+                            echo "<a class='num_pg' href='vagas.php?pagina=$i'>$i</a>";
+                
+                        }
+                    }
+                    echo "<a class='ult_pg' href='vagas.php?pagina=$qtd_pg'>Último</a>";
+                    echo "</div>";
+
+                ?>
                     
                 </div>
                 <div class='div_lateral'>
@@ -148,16 +160,30 @@
 
                     <!--Pesquisa lateral -->
                     <div class="form-lateral">
-                        <h2>Buscar vagas</h2>
-                        <!--Fazer o back-end -->
-                        <form action="pesquisa.php" method="get" name='form_pesquisar'>
-                            <div class="box_pesquisa">
-                                <input type="text" name="f_name" placeholder="Busque vagas pelo nome" required="required">
-                                <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill-rule="evenodd" d="M14.53 15.59a8.25 8.25 0 111.06-1.06l5.69 5.69a.75.75 0 11-1.06 1.06l-5.69-5.69zM2.5 9.25a6.75 6.75 0 1111.74 4.547.746.746 0 00-.443.442A6.75 6.75 0 012.5 9.25z"></path></svg></button>
-                            </div>
-                        </form>
-                        
-                    </div><!--Fim da pesquisa lateral -->
+                    <h2>Buscar vagas</h2>
+                    <!--Fazer o back-end -->
+                    <?php 
+
+                        if(!isset($_SESSION['numLogin'])){
+                            
+                            echo "<form action='pesquisa.php' method='get' name='form_pesquisar'>
+                                    <div class='box_pesquisa'>
+                                        <input type='text' name='f_name' placeholder='Busque vagas pelo nome' required='required'>
+                                        <button><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'><path fill-rule='evenodd' d='M14.53 15.59a8.25 8.25 0 111.06-1.06l5.69 5.69a.75.75 0 11-1.06 1.06l-5.69-5.69zM2.5 9.25a6.75 6.75 0 1111.74 4.547.746.746 0 00-.443.442A6.75 6.75 0 012.5 9.25z'></path></svg></button>
+                                    </div>
+                                 </form>";
+                        }else{
+
+                            echo "<form action='pesquisa.php?num=".$_SESSION['numLogin']."' method='POST' name='form_pesquisar'>
+                                    <div class='box_pesquisa'>
+                                        <input type='text' name='f_name' placeholder='Busque vagas pelo nome' required='required'>
+                                        <button><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'><path fill-rule='evenodd' d='M14.53 15.59a8.25 8.25 0 111.06-1.06l5.69 5.69a.75.75 0 11-1.06 1.06l-5.69-5.69zM2.5 9.25a6.75 6.75 0 1111.74 4.547.746.746 0 00-.443.442A6.75 6.75 0 012.5 9.25z'></path></svg></button>
+                                    </div>
+                                </form>";
+
+                        }
+                    ?>                    
+                </div><!--Fim da pesquisa lateral -->
                     </aside>
                     <!--CONTEUDO LATERAL -->
                     <aside class='lateral'>
--- vagas.php
+++ vagas.php
@@ -143,16 +143,30 @@
                 <aside class='lateral'>
                     <!--Pesquisa lateral -->
                     <div class="form-lateral">
-                        <h2>Buscar vagas</h2>
-                        <!--Fazer o back-end -->
-                        <form action="pesquisa.php" method="get" name='form_pesquisar'>
-                            <div class="box_pesquisa">
-                                <input type="text" name="f_name" placeholder="Busque vagas pelo nome" required="required">
-                                <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill-rule="evenodd" d="M14.53 15.59a8.25 8.25 0 111.06-1.06l5.69 5.69a.75.75 0 11-1.06 1.06l-5.69-5.69zM2.5 9.25a6.75 6.75 0 1111.74 4.547.746.746 0 00-.443.442A6.75 6.75 0 012.5 9.25z"></path></svg></button>
-                            </div>
-                        </form>
-                        
-                    </div><!--Fim da pesquisa lateral -->
+                    <h2>Buscar vagas</h2>
+                    <!--Fazer o back-end -->
+                    <?php 
+
+                        if(!isset($_SESSION['numLogin'])){
+                            
+                            echo "<form action='pesquisa.php' method='get' name='form_pesquisar'>
+                                    <div class='box_pesquisa'>
+                                        <input type='text' name='f_name' placeholder='Busque vagas pelo nome' required='required'>
+                                        <button><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'><path fill-rule='evenodd' d='M14.53 15.59a8.25 8.25 0 111.06-1.06l5.69 5.69a.75.75 0 11-1.06 1.06l-5.69-5.69zM2.5 9.25a6.75 6.75 0 1111.74 4.547.746.746 0 00-.443.442A6.75 6.75 0 012.5 9.25z'></path></svg></button>
+                                    </div>
+                                 </form>";
+                        }else{
+
+                            echo "<form action='pesquisa.php?num=".$_SESSION['numLogin']."' method='POST' name='form_pesquisar'>
+                                    <div class='box_pesquisa'>
+                                        <input type='text' name='f_name' placeholder='Busque vagas pelo nome' required='required'>
+                                        <button><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'><path fill-rule='evenodd' d='M14.53 15.59a8.25 8.25 0 111.06-1.06l5.69 5.69a.75.75 0 11-1.06 1.06l-5.69-5.69zM2.5 9.25a6.75 6.75 0 1111.74 4.547.746.746 0 00-.443.442A6.75 6.75 0 012.5 9.25z'></path></svg></button>
+                                    </div>
+                                </form>";
+
+                        }
+                    ?>                    
+                </div><!--Fim da pesquisa lateral -->
                 </aside>       
                 <!--CONTEUDO LATERAL -->
                 <aside class='lateral'>
